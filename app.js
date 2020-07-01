59a4b6328649dfb48d98fc5725922ed0fbbe49e1
VG <vgatera@gmail.com>
2020-07-01 21:06:23 +0200

    register validate

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+//Import routes
+const authRoute = require("./routes/auth");
 
 const app = express();
 const port = 3000;
+dotenv.config();
 
-//Import routes
-const authRoute = require("./routes/auth");
+//Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("connected to DB")
+);
+//Middleware
+app.use(express.json());
 
 //Route middlewares
 //this sets the fixed path to http://localhost/api/user
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,31 @@
 const router = require("express").Router();
+const User = require("../model/User");
 
-router.post("/register", (req, res) => res.send("Register"));
+//Validation
+const Joi = require("@hapi/joi");
+
+const userValidate = Joi.object({
+  name: Joi.string().min(3).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(8).required(),
+});
+
+router.post("/register", async (req, res) => {
+  //Validate data before creating user
+  const { error } = userValidate.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 module.exports = router;
